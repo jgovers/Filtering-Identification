01e0835a0e7c4ae018d6113460787d60cf2051c4
jgovers <j.a.govers@gmail.com>
2018-01-03 17:00:32 +0100

    Added first two functions

---
+++ AOloopMVM.m
@@ -0,0 +1,5 @@
+function [var_eps] = AOloopMVM(G,H,C_phi0,sig_e,phi);
+    
+    var_eps = sig_e+phi;
+    
+end
--- TurbMod.m
+++ TurbMod.m
@@ -5,9 +5,25 @@
 clear all
 close all
 clc
+
 %% Load data
 load('systemMatrices.mat')
  myfile = 'turbulenceData.mat';
  [parentdir,~,~]=fileparts(pwd);
  load(fullfile(parentdir,myfile))
-%% 3.1 Reconstruct the waveform
+ 
+%% 3.6
+C_phi0  = 0;
+sig_e   = 0;
+phi     = 0;
+
+[var_eps] = AOloopMVM(G,H,C_phi0,sig_e,phi);
+
+%% No Control
+sigma = zeros(size(phiSim));
+for i = 1:size(phiSim,2)
+    phik = phiSim{i};
+    [sigma(i)] = AOloop_nocontrol(phik,SNR,H,G);
+end
+
+plot(sigma)
