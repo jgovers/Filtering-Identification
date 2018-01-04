71993cee30baa900c7af43a891c04dd8bdd9edde
DESKTOP-VA2F7BS\Niels <niels.uit@gmail.com>
2018-01-04 10:14:46 +0100

    Niks nieuws, moest committen om te pullen.

--- TurbMod.m
+++ TurbMod.m
@@ -1,13 +1,19 @@
 %% Final Exercise Filtering & Identification
 % Niels Uitterdijk  4276892
 % Jurriaan Govers   4163753
-
 clear all
 close all
 clc
 %% Load data
 load('systemMatrices.mat')
  myfile = 'turbulenceData.mat';
- [parentdir,~,~]=fileparts(pwd);
- load(fullfile(parentdir,myfile))
+[parentdir,~,~]=fileparts(pwd);
+load(fullfile(parentdir,myfile))
 %% 3.1 Reconstruct the waveform
+% function [ sigma ] = AOloop_nocontrol(phik,SNR,H,G)
+
+
+
+[sigma] = AOloop_nocontrol(phiSim{1,1},SNR,H,G);
+
+%% Test test
