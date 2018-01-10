0ba27b386c5424d2bf77223f3b70fae89abc543f
DESKTOP-VA2F7BS\Niels <niels.uit@gmail.com>
2018-01-10 14:59:50 +0100

    Nog een cleanup

--- AOloop_nocontrol.m
+++ AOloop_nocontrol.m
@@ -4,16 +4,12 @@ n = size(H,1);      % dimension lifted wavefront
 ns = size(G,1);     % dimension lifted sensor slopes
 T = length(phik);   % number of temporal phase points
 epsk = zeros(n,T);  % residual wavefront
-sk = zeros(ns,T);   % slopes measurements
 sigma = zeros(T,1);
-
 for k = 1:T-1
     epsk(:,k+1) = phik(:,k+1);
-    sk(:,k+1) = awgn(G*epsk(:,k+1),SNR);
     sigma(k+1) = var(epsk(:,k+1)-mean(epsk(:,k+1));
 end
 sigma = mean(sigma);
-
 end
 
 
