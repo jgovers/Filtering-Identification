b08aeb6908051b6f335137bc278e49efcaace94f
DESKTOP-VA2F7BS\Niels <niels.uit@gmail.com>
2018-01-15 09:40:51 +0100

    no message

--- AOloopAR.m
+++ AOloopAR.m
@@ -1,11 +1,4 @@
 function [var_eps] = AOloopAR(G,H,C_phi0,C_phi1,sig_e,phik)
-%% Wat te doen
-% 0. Find out how to add white noise with covariance MATRIX
-% 1. Generate s(k) from phi(k) by s(k) = G phi(k) + e(k)~(0,sigma_e^2).
-% epsh(k+1) = C_phi\C_phi1*eps(k) + [AH -H][u(k-1) u(k)]' + K(y(k) - C eps(k))
-
-% eps(k+1) = C_phi\C_phi1*eps(k) + [AH -H][u(k-1) u(k)]' + w(k)
-% s(k) = G eps(k) + [GH 0][u(k-1) u(k)]' + e(k)
 %% Initialising matrices
 n = size(H,1);          % State dimension 
 ns = size(G,1);         % Output dimension
@@ -19,17 +12,15 @@ u_k = zeros(n,T);       % Optimal control action
 [A,Cw,K] = computeKalmanAR(C_phi0,C_phi1,G,sig_e);
 B = [A*H -H];
 D = [G*H zeros(72,49)];
-vare = -log10(sig_e)*10;
+vare = -log10(sig_e)*20;
 varW = 0;
-%% Generation of y data
-s = awgn(G*phik,vare);
-%% Eerst epshat, dan gebruiken voor u. vervolgens var berekenen van var van epshat.
+%% Control loop
 for k = 2:T-2
     %% System equations
     eps(:,k) = phik(:,k) - H*u_k(:,k);
-    s = awgn(G*eps(:,k),SNR);
+    s = awgn(G*eps(:,k),vare);
     %% Observer and controller
-    eps_h(:,k+1) = (A-K*G)*eps_h(:,k) + B*[u_k(:,k-1); u_k(:,k)] + K*sk(:,k);
+    eps_h(:,k+1) = (A-K*G)*eps_h(:,k) + B*[u_k(:,k-1); u_k(:,k)] + K*s;
     u_k(:,k+1) = u_k(:,k) + H\eps_h(:,k+1);
     %% Variance
     sigma(k+1) = var(eps(:,k));
--- TurbMod.m
+++ TurbMod.m
@@ -1,12 +1,10 @@
 %% Final Exercise Filtering & Identification
 % Niels Uitterdijk  4276892
 % Jurriaan Govers   4163753
-
 clear all
 close all
 clc
 tic
-
 %% Load data
 fprintf('Loading data:\n')
 load('systemMatrices.mat')
